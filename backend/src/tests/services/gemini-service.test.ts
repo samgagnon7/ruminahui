bc70a6aeab065abdd823d7b2e195fd83efce8fc9
samgamji <samgagnon7@gmail.com>
2024-10-11 08:26:12 -0400

    added mocha test to enable testing with base64 directly in code

---
+++ backend/src/tests/services/gemini-service.test.ts
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import { generateContent } from '../../services/gemini-service';
+import fs from 'fs';
+
+describe('gemini service', () => {
+  it('generates content correctly from image', async () => {
+    const imageBase64 = fs.readFileSync('/Users/samgamji/Code/ruminahui/backend/src/tests/test-files/mahjong-base64-jpeg.txt', 'utf-8');
+    const result = await generateContent(imageBase64);
+    console.log(result);
+  });
+});
